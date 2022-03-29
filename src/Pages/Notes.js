9569376740e1c630125b9467620ae08cc998c33d
mastanpatel <Bushra.mateen95@outlook.com>
2022-03-29 13:15:14 +0530

    added components

---
+++ src/Pages/Notes.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../Components/Header'
+import ListItem from '../Components/ListItem'
+import AddButton from '../Components/AddButton'
+
+
+export default function Notes() {
+  return (
+    <div>
+    <Header />
+    <ListItem />
+    <AddButton />
+    </div>
+  )
+}
